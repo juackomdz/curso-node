ad75512a638d353b2272e9a172057e43569095e0
Joaquin Mendez <joaquin.enmt@gmail.com>
2025-07-08 11:09:28 -0400

    Feat: Metodos Put y DELETE para categorias

--- express-postgres/routes/categoria.js
+++ express-postgres/routes/categoria.js
@@ -33,4 +33,34 @@ router.get("/:id", (req, res) => {
         .catch(e => res.status(400).json(e))
 })
 
+router.put("/:id", (req, res) => {
+
+    let id = parseInt(req.params.id)
+
+    prisma.categoria.update({
+        where: {
+            id: id
+        },
+        data: {
+            nombre: req.body.nombre
+        }
+    })
+        .then(res.status(200).json({ mensaje: "actualizado con exito" }))
+        .catch(e => res.status(400).json(e))
+})
+
+router.delete("/:id", (req, res) => {
+
+    let id = parseInt(req.params.id)
+
+    prisma.categoria.delete({
+        where: {
+            id: id
+        }
+    })
+        .then(res.status(200).json({ mensaje: "eliminado con exito" }))
+        .catch(e => res.status(400).json(e))
+
+})
+
 export default router
